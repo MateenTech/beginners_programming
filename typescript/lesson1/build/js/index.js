ec45be6201957cb0211ade2cfc783966e27c2aec
Mateen Shahbaz Ahmed <75861907+MateenAhmed10@users.noreply.github.com>
2023-06-04 10:38:38 +0500

    typescript

---
+++ typescript/lesson1/build/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Primitivies data type
+Object.defineProperty(exports, "__esModule", { value: true });
+let animal = "Dog";
+let breed = "Golden Retriever";
---
+++ typescript/lesson1/src/index.ts
@@ -0,0 +1,4 @@
+// Primitivies data type
+
+let animal: string = "Dog";
+let breed: string = "Golden Retriever";
